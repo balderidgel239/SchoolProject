e02abd1e34b20d7289b5b25c67a08ebf77771f57
balderidgel239 <balderidgel239@gmail.com>
2025-03-12 12:00:42 +0000

    Add 7.php

---
+++ src/7.php
@@ -0,0 +1,9 @@
+<?php
+$schoolProjects = array(
+    array("projectName" => "School Project #1", "description" => "Project Description #1"),
+    array("projectName" => "School Project #2", "description" => "Project Description #2"),
+    array("projectName" => "School Project #3", "description" => "Project Description #3")
+);
+
+return $schoolProjects;
+?>
