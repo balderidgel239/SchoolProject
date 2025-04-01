79e418bbb8b9d72e49fe6c3e2cd4f906545a72a0
balderidgel239 <balderidgel239@gmail.com>
2025-04-01 14:30:33 +0000

    Add 22.php

---
+++ src/22.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants
+$var1 = 'value1';
+$var2 = 'value2';
+
+// Example code that uses variables
+
+// Output the values of variables
+echo "Variable 1: $var1\n";
+echo "Variable 2: $var2";
+
+?>
